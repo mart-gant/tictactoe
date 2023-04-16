d234b28b788d10312922aad4714ceb9055dee17a
mart-gant <martgant@gmail.com>
2023-04-16 13:16:09 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,251 +1,109 @@
-import android.os.Build.VERSION_CODES.R
-import androidx.appcompat.app.AppCompatActivity
+package com.example.tictactoe
+
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import com.example.tictactoe.R
-import java.util.*
-import kotlin.collections.ArrayList
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+class MainActivity : AppCompatActivity() {
+    var gameActive = true
+    var activePlayer = 0
+    var gameState = intArrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
 
-class MainActivity(
-    val bu2: Button?,
-    val bu3: Button?,
-    val bu4: Button?,
-    val bu5: Button?,
-    val bu6: Button?,
-    val bu7: Button?,
-    val bu8: Button?,
-    val bu9: Button?,
-    val bu1: Button?
-) : AppCompatActivity() {
+    var winPositions = arrayOf(
+        intArrayOf(0, 1, 2),
+        intArrayOf(3, 4, 5),
+        intArrayOf(6, 7, 8),
+        intArrayOf(0, 3, 6),
+        intArrayOf(1, 4, 7),
+        intArrayOf(2, 5, 8),
+        intArrayOf(0, 4, 8),
+        intArrayOf(2, 4, 6)
+    )
+
+    fun playerTap(view: View) {
+        val img = view as ImageView
+        val tappedImage = img.tag.toString().toInt()
+
+        if (!gameActive) {
+            gameReset(view)
+        }
+
+        if (gameState[tappedImage] == 2) {
+            counter++
+
+            if (counter == 9) {
+                gameActive = false
+            }
+
+            gameState[tappedImage] = activePlayer
+
+            img.translationY = -1000f
+
+            if (activePlayer == 0) {
+                img.setImageResource(R.drawable.x)
+                activePlayer = 1
+                val status = findViewById<TextView>(R.id.status)
+
+                status.text = "O's Turn - Tap to play"
+            } else {
+                img.setImageResource(R.drawable.o)
+                activePlayer = 0
+                val status = findViewById<TextView>(R.id.status)
+
+                status.text = "X's Turn - Tap to play"
+            }
+            img.animate().translationYBy(1000f).duration = 300
+        }
+        var flag = 0
+        for (winPosition in winPositions) {
+            if (gameState[winPosition[0]] == gameState[winPosition[1]] && gameState[winPosition[1]] == gameState[winPosition[2]] && gameState[winPosition[0]] != 2) {
+                flag = 1
+
+                var winnerStr: String
+
+                gameActive = false
+                winnerStr = if (gameState[winPosition[0]] == 0) {
+                    "X has won"
+                } else {
+                    "O has won"
+                }
+                val status = findViewById<TextView>(R.id.status)
+                status.text = winnerStr
+            }
+        }
+        if (counter == 9 && flag == 0) {
+            val status = findViewById<TextView>(R.id.status)
+            status.text = "Match Draw"
+        }
+    }
+
+    fun gameReset(view: View?) {
+        gameActive = true
+        activePlayer = 0
+        for (i in gameState.indices) {
+            gameState[i] = 2
+        }
+        (findViewById<View>(R.id.imageView0) as ImageView).setImageResource(0)
+        (findViewById<View>(R.id.imageView1) as ImageView).setImageResource(0)
+        (findViewById<View>(R.id.imageView2) as ImageView).setImageResource(0)
+        (findViewById<View>(R.id.imageView3) as ImageView).setImageResource(0)
+        (findViewById<View>(R.id.imageView4) as ImageView).setImageResource(0)
+        (findViewById<View>(R.id.imageView5) as ImageView).setImageResource(0)
+        (findViewById<View>(R.id.imageView6) as ImageView).setImageResource(0)
+        (findViewById<View>(R.id.imageView7) as ImageView).setImageResource(0)
+        (findViewById<View>(R.id.imageView8) as ImageView).setImageResource(0)
+        val status = findViewById<TextView>(R.id.status)
+        status.text = "X's Turn - Tap to play"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-
-    fun buClick( view:View){
-
-
-        val buSelected = view as Button
-
-        var cellId = 0
-        when(buSelected.id){
-            R.id.bu1 -> cellId = 1
-            R.id.bu2 -> cellId = 2
-            R.id.bu3 -> cellId = 3
-            R.id.bu4 -> cellId = 4
-            R.id.bu5 -> cellId = 5
-            R.id.bu6 -> cellId = 6
-            R.id.bu7 -> cellId = 7
-            R.id.bu8 -> cellId = 8
-            R.id.bu9 -> cellId = 9
-        }
-
-        playGame(cellId,buSelected)
-
+    companion object {
+        var counter = 0
     }
-
-    var activePlayer = 1
-
-    var player1 = ArrayList<Int>()
-    var player2 = ArrayList<Int>()
-
-    fun playGame(cellId:Int, buSelected:Button){
-
-
-        if( activePlayer == 1 ){
-            buSelected.text = "X"
-            buSelected.setBackgroundResource(R.color.blue)
-            player1.add(cellId)
-            activePlayer = 2
-            autoPlay()
-
-        }else{
-
-            buSelected.text = "O"
-            buSelected.setBackgroundResource(R.color.darkGreen)
-            player2.add(cellId)
-            activePlayer = 1
-
-        }
-
-        buSelected.isEnabled = false
-
-        checkWinner()
-    }
-
-
-    fun checkWinner() {
-
-        var winer = -1
-
-
-        // row 1
-        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
-            winer = 1
-        }
-        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
-            winer = 2
-        }
-
-
-        // row 2
-        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
-            winer = 1
-        }
-        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
-            winer = 2
-        }
-
-        // row 3
-        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
-            winer = 1
-        }
-        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
-            winer = 2
-        }
-
-
-        // col 1
-        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
-            winer = 1
-        }
-        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
-            winer = 2
-        }
-
-
-        // col 2
-        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
-            winer = 1
-        }
-        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
-            winer = 2
-        }
-
-
-        // col 3
-        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
-            winer = 1
-        }
-        if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
-            winer = 2
-        }
-
-
-
-        if (winer == 1) {
-            player1WinsCounts += 1
-            Toast.makeText(this, "Player 1 win the game", Toast.LENGTH_LONG).show()
-            restartGame()
-
-        } else if (winer == 2) {
-            player2WinsCounts += 1
-            Toast.makeText(this, "Player 2 win the game", Toast.LENGTH_LONG).show()
-            restartGame()
-        }
-
-
-    }
-
-    fun autoPlay(){
-
-
-        var emptyCells = ArrayList<Int>()
-
-        for( cellId in 1..9){
-
-            if( !(player1.contains(cellId) || player2.contains(cellId))){
-                emptyCells.add(cellId)
-            }
-        }
-
-
-
-        if(emptyCells.size==0){
-            restartGame()
-        }
-
-
-        val r = Random()
-        val randIndex = r.nextInt(emptyCells.size )
-        val cellId = emptyCells[randIndex]
-
-        var buSelected:Button?
-        buSelected =  when(cellId){
-            1-> bu1
-            2-> bu2
-            3-> bu3
-            4-> bu4
-            5-> bu5
-            6-> bu6
-            7-> bu7
-            8-> bu8
-            9-> bu9
-            else ->{ bu1}
-
-        }
-
-        if (buSelected != null) {
-            playGame(cellId, buSelected)
-        }
-
-    }
-
-
-
-    var player1WinsCounts = 0
-    var player2WinsCounts = 0
-
-    fun restartGame(){
-
-        activePlayer = 1
-        player1.clear()
-        player2.clear()
-
-        for(cellId in 1..9){
-
-            var buSelected:Button? = when(cellId){
-                1-> bu1
-                2-> bu2
-                3-> bu3
-                4-> bu4
-                5-> bu5
-                6-> bu6
-                7-> bu7
-                8-> bu8
-                9-> bu9
-                else ->{ bu1}
-
-            }
-            buSelected!!.text = ""
-            buSelected!!.setBackgroundResource(R.color.whileBu)
-            buSelected!!.isEnabled = true
-        }
-
-        Toast.makeText(this,"Player1: $player1WinsCounts, Player2: $player2WinsCounts", Toast.LENGTH_LONG).show()
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
